d1b3cb9405e9cb57638395523e2baa809a2d7e67
SHANMUKHA REDDY VASA <113333841+shanmu97@users.noreply.github.com>
2025-02-14 16:40:40 +0530

    Update UserController.js phone

--- Backend/Controller/UserController.js
+++ Backend/Controller/UserController.js
@@ -63,6 +63,7 @@ const loginUser = asyncHandler(async (req,res) =>{
             name:user.name,
             email:user.email,
             role:user.role,
+            phone:user.phone,
             token:generateToken(user.id)
         })
     }else{
